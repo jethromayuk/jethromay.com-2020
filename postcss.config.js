87a01baa2580a30a393be60474116cbe638fb5bb
Jethro May <jethro@plusnarrative.com>
2020-05-16 07:23:34 +0200

    Updated config, added config to netlify config file, added postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
-        require('postcss-import'),
         require('tailwindcss'),
+        require('postcss-nested'),
         require('autoprefixer'),
     ]
 };
