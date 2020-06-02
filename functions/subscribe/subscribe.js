ad0a4cec0fad425f58f81cdb51fc7fd047877b9b
Jethro May <jethro@plusnarrative.com>
2020-06-02 13:44:52 +0200

    Corrected functions

--- functions/subscribe/subscribe.js
+++ functions/subscribe/subscribe.js
@@ -1,24 +1,32 @@
 const fetch = require('node-fetch');
-const base64 = require('base-64'); exports.handler = async (event, context) => {
+const base64 = require('base-64');
+
+exports.handler = async (event, context) => {
     // Only allow POST
     if (event.httpMethod !== 'POST') {
         return { statusCode: 405, body: 'Method Not Allowed' };
     }  const errorGen = msg => {
         return { statusCode: 500, body: msg };
     };   try {
-        const { email } = JSON.parse(event.body);    if (!email) {
+        const { email } = JSON.parse(event.body);
+        if (!email) {
             return errorGen('Missing Email');
-        }     const subscriber = {
+        }
+        const subscriber = {
             email_address: email,
             status: 'subscribed',
-        };     const creds = `any:${process.env.MAILCHIMP_KEY}`;    const response = await fetch('https://us20.api.mailchimp.com/3.0/lists/9fae1f7a02/members/', {
+        };
+        const creds = `any:${process.env.MAILCHIMP_KEY}`;
+        const response = await fetch('https://us20.api.mailchimp.com/3.0/lists/9fae1f7a02/members/', {
             method: 'POST',
             headers: {
                 Accept: '*/*',
                 'Content-Type': 'application/json',
                 Authorization: `Basic ${base64.encode(creds)}`, },
             body: JSON.stringify(subscriber),
-        });     const data = await response.json();
+        });
+
+        const data = await response.json();
 
         if (!response.ok) {
             // NOT res.status >= 200 && res.status < 300
